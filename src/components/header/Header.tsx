80b9f834d7b3838fdda5d9714838842a5986aabf
Sina Ghadri <sina.ghadri.co@gmail.com>
2021-12-23 14:45:33 +0330

    fix styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,7 @@ import { AccountDetails } from "./account-details";
 import { ConnectWallet } from "./connect-wallet";
 import { WalletOptions } from "components/wallet-options";
 import { Modal } from "ui-components";
-
+import { ReactComponent as TotemLogo } from "assets/icons/coins/totem.svg";
 import { useGlobalState, useGlobalDispatch } from "states/globalContext";
 // import { ThemeSelector } from "./theme-selector";
 import "./Header.scss";
@@ -13,10 +13,12 @@ const Header: FC = () => {
     const globalDispatch = useGlobalDispatch();
     return (
         <div className="header">
+            <div className="header-title">
+                <TotemLogo width="50" /> Staking
+            </div>
             <ConnectWallet>
                 <AccountDetails />
             </ConnectWallet>
-
             <Modal
                 title="Connect Wallet :"
                 open={walletOptions}
@@ -26,7 +28,6 @@ const Header: FC = () => {
             >
                 <WalletOptions />
             </Modal>
-
             {/* <ThemeSelector /> */}
         </div>
     );
